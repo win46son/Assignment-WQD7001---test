607e0276920902b9e104d5aca87d2a4bc8001f7f
DESKTOP-5V90H0E\winso <winson_2397@hotmail.com>
2024-06-01 00:32:22 +0800

    sixth commit

--- predict.py
+++ predict.py
@@ -2,13 +2,19 @@ import streamlit as st
 from model import show_predict_page
 from explore import show_explore_page, show_churn_reasons, show_service_factors, show_high_corr
 
+text = """
+    <div style='text-align: justify; font-family: "Amasis MT Pro Black", sans-serif; font-weight: bold; font-size: 25px;'>
+    CHURN SHIELD
+    </div>
+"""
+st.sidebar.markdown(text, unsafe_allow_html=True)
 page = st.sidebar.selectbox('Prediction or Exploration', ('Prediction','Exploration'))
 
 if page == 'Prediction':
     show_predict_page()
 elif page == 'Exploration':
     show_explore_page()
-    choice = st.sidebar.selectbox('Please Choose',('All','Main Churn Reasons', 'Service Factors','Dummy Correlation'))
+    choice = st.sidebar.selectbox('Sector:',('All','Main Churn Reasons', 'Service Factors','Dummy Correlation'))
     if choice == 'All':
         show_churn_reasons()
         st.divider()
